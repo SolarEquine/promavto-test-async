bd521d00721f723b03af9ecb80004745ac0e6cb3
Tia <nik12346@yandex.ru>
2023-01-21 18:22:09 +0300

    redo fetchPosts with try catch and add loader

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 const url = "https://jsonplaceholder.typicode.com/posts";
-
-async function sendRequest(url){
-    return fetch(url).then(response => {
-        return response.text()
-    })
+const btn = document.querySelector(".btn")
+const postsElem = document.querySelector(".posts");
+const loader = document.createElement('div');
+loader.classList.add("loader");
+let posts = []
+btn.addEventListener("click", ()=>{fetchPosts(url)});
+async function fetchPosts(url){
+    try{
+        postsElem.prepend(loader);
+        const responce =  await fetch(url);
+        posts = await responce.json();
+    }
+    catch(e){
+        alert(e)
+    }
+    finally{
+        postsElem.removeChild(loader);
+        updatePosts();
+    }
 }
 
-sendRequest(url).then(data=>console.log(data))
-.catch(err=>console.log(err));
+function updatePosts(){
+    posts.forEach(post => {
+        const title = post.title;
+        const body = post.body;
+        const postElem = document.createElement("div");
+        const titleElem = document.createElement("div");
+        const bodyElem = document.createElement("div");
+        postElem.classList.add("post");
+        titleElem.classList.add("post_title");
+        bodyElem.classList.add("post_body");
+        titleElem.innerHTML = title;
+        bodyElem.innerHTML = body;
+        postElem.appendChild(titleElem);
+        postElem.appendChild(bodyElem);
+        postsElem.appendChild(postElem);
+    });
+}
